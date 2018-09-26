725feb08d086db3c1339c3f5c9631620811c4ad3
Ruben <Ruben@DESKTOP-H1AMC9R>
2018-09-26 21:11:16 +0200

    Eliminado import no usado

--- AccesoDatosUF1AC02/src/com/rfm/utils/Factory.java
+++ AccesoDatosUF1AC02/src/com/rfm/utils/Factory.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
-import com.rfm.model.Persona;
-
 public interface Factory extends Serializable {
 
 	public File writeFile(String fileName, List<?> lista) throws IOException;
