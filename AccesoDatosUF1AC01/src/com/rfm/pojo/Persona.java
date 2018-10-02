119be53bdb2ed40c6b343cc5e8bc08ebe2b79d39
loqum <ruben693@gmail.com>
2018-10-02 17:35:39 +0200

    implementado métodos hash y equals

--- AccesoDatosUF1AC01/src/com/rfm/pojo/Persona.java
+++ AccesoDatosUF1AC01/src/com/rfm/pojo/Persona.java
@@ -1,15 +1,15 @@
 package com.rfm.pojo;
 
 public class Persona {
-	
+
 	private String nombre;
 	private String apellido;
 	private String idioma;
 	private String usuario;
 	private String contrasenya;
-	
+
 	public Persona() {
-		
+
 	}
 
 	public Persona(String nombre, String apellido, String idioma, String usuario, String contrasenya) {
@@ -62,9 +62,58 @@ public class Persona {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((contrasenya == null) ? 0 : contrasenya.hashCode());
+		result = prime * result + ((idioma == null) ? 0 : idioma.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (contrasenya == null) {
+			if (other.contrasenya != null)
+				return false;
+		} else if (!contrasenya.equals(other.contrasenya))
+			return false;
+		if (idioma == null) {
+			if (other.idioma != null)
+				return false;
+		} else if (!idioma.equals(other.idioma))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", idioma=" + idioma + ", usuario=" + usuario
 				+ ", contrasenya=" + contrasenya + "]";
 	}
-	
+
 }
