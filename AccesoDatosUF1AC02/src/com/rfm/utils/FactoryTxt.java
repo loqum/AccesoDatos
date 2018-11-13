2af6ec963c1337f3fbd3e75d65df4158b0fb9720
loqum <ruben693@gmail.com>
2018-11-13 12:01:56 +0100

    Implementado serializable

--- AccesoDatosUF1AC02/src/com/rfm/utils/FactoryTxt.java
+++ AccesoDatosUF1AC02/src/com/rfm/utils/FactoryTxt.java
@@ -6,85 +6,86 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.util.List;
 
 import com.rfm.model.Persona;
 
-public class FactoryTxt implements Factory {
-	private static final long serialVersionUID = 2385994570516030149L;
+public class FactoryTxt implements Factory, Serializable {
+  private static final long serialVersionUID = 2385994570516030149L;
 
-	FactoryUtil createFile = new FactoryUtil();
-	FileReader fileReader = null;
-	BufferedReader bufferedReader = null;
-	FileWriter fileWriter = null;
-	PrintWriter printWriter = null;
-	File file = null;
-	Persona persona = null;
+  FactoryUtil createFile = new FactoryUtil();
+  FileReader fileReader = null;
+  BufferedReader bufferedReader = null;
+  FileWriter fileWriter = null;
+  PrintWriter printWriter = null;
+  File file = null;
+  Persona persona = null;
 
-	public FactoryTxt() {
-		super();
-	}
+  public FactoryTxt() {
+    super();
+  }
 
-	@Override
-	public File writeFile(String fileName, List<?> personas) throws IOException {
+  @Override
+  public File writeFile(String fileName, List<?> personas) throws IOException {
 
-		file = FactoryUtil.create(fileName);
+    file = FactoryUtil.create(fileName);
 
-		try {
-			fileWriter = new FileWriter(file);
-			printWriter = new PrintWriter(fileWriter);
+    try {
+      fileWriter = new FileWriter(file);
+      printWriter = new PrintWriter(fileWriter);
 
-			printWriter.println(personas.toString());
+      printWriter.println(personas.toString());
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw e;
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw e;
 
-		} finally {
-			try {
-				if (null != fileWriter) {
-					fileWriter.close();
-				}
-			} catch (IOException e2) {
-				e2.printStackTrace();
-				throw e2;
-			}
-		}
+    } finally {
+      try {
+        if (null != fileWriter) {
+          fileWriter.close();
+        }
+      } catch (IOException e2) {
+        e2.printStackTrace();
+        throw e2;
+      }
+    }
 
-		return file;
-	}
+    return file;
+  }
 
-	@Override
-	public String readFile(String fileName) throws IOException {
+  @Override
+  public String readFile(String fileName) throws IOException {
 
-		String linea;
+    String linea;
 
-		try {
-			fileReader = new FileReader(fileName);
-			bufferedReader = new BufferedReader(fileReader);
+    try {
+      fileReader = new FileReader(fileName);
+      bufferedReader = new BufferedReader(fileReader);
 
-			while ((linea = bufferedReader.readLine()) != null) {
-				System.out.println(linea);
-			}
+      while ((linea = bufferedReader.readLine()) != null) {
+        System.out.println(linea);
+      }
 
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			throw e1;
+    } catch (IOException e1) {
+      e1.printStackTrace();
+      throw e1;
 
-		} finally {
-			try {
+    } finally {
+      try {
 
-				if (null != fileReader) {
-					fileReader.close();
-				}
+        if (null != fileReader) {
+          fileReader.close();
+        }
 
-			} catch (Exception e2) {
-				e2.printStackTrace();
-				throw e2;
-			}
-		}
+      } catch (Exception e2) {
+        e2.printStackTrace();
+        throw e2;
+      }
+    }
 
-		return linea;
-	}
+    return linea;
+  }
 
 }
